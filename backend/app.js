0d870189b35c8269cf5d1b3810092d331ffc0622
Fayyaz ud-din <111335793+Fayyaz42401@users.noreply.github.com>
2023-06-16 10:09:04 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "https://fayyaz-social-media.netlify.app",
     credentials: true,
   })
 );

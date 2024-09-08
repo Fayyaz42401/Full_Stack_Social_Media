fa51fad941d26d82599bba63551de1581118016e
Fayyaz ud-din <111335793+Fayyaz42401@users.noreply.github.com>
2024-09-08 16:37:27 +0500

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,2 +1,2 @@
-export const USER_API = `https://full-stack-social-media.vercel.app/api/v1/user`;
-export const POST_API = `https://full-stack-social-media.vercel.app/api/v1/post`;
+export const USER_API = `https://full-stack-social-media-9l28.vercel.app/api/v1/user`;
+export const POST_API = `https://full-stack-social-media-9l28.vercel.app/api/v1/post`;
